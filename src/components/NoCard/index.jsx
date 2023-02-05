d46777ba4c630a517a463babb914f40cc118a2ab
caiquepb <caiquepbatistaa@gmail.com>
2023-02-05 20:36:32 -0300

    Entrega v3.2/fix responsividade

--- src/components/NoCard/index.jsx
+++ src/components/NoCard/index.jsx
@@ -2,9 +2,9 @@ import NoCardImage from "../../assets/NoCard.svg";
 import "./styles.css";
 
 export const NoCard = () => (
-  <section className="sectionPage__container">
-    <h1 className="sectionPage__title">Reusumo financeiro</h1>
-    <p className="sectionPage__paragraph">
+  <section className="noCard__container">
+    <h1 className="noCard__title">Reusumo financeiro</h1>
+    <p className="noCard__paragraph">
       Você ainda não poussui nenhum lançamento
     </p>
     <img src={NoCardImage} alt="No Card Image" />
